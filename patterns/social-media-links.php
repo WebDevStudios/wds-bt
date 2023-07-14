cf78afd61f496f0d38a78718e69ae4009af127c1
Brian Gardner <bmg1227@gmail.com>
2023-07-14 06:31:33 -0500

    Add Social Media Links patterns.

---
+++ patterns/social-media-links.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Title: Social media links.
+ * Slug: powder/social-media-links
+ * Description: Row of social media links.
+ * Categories: social-media
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"30px","left":"30px"},"margin":{"top":"0px"}}},"layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="margin-top:0px;padding-right:30px;padding-left:30px">
+	<!-- wp:group {"align":"wide","style":{"typography":{"lineHeight":"1"},"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small"},"blockGap":"var:preset|spacing|x-small"},"border":{"top":{"color":"var:preset|color|contrast","width":"1px"},"bottom":{"color":"var:preset|color|contrast","width":"1px"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"},"fontSize":"max-36"} -->
+	<div class="wp-block-group alignwide has-max-36-font-size" style="border-top-color:var(--wp--preset--color--contrast);border-top-width:1px;border-bottom-color:var(--wp--preset--color--contrast);border-bottom-width:1px;padding-top:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);line-height:1">
+		<!-- wp:paragraph {"className":"is-style-no-underline"} -->
+		<p class="is-style-no-underline"><a href="https://twitter.com/">Twitter</a></p>
+		<!-- /wp:paragraph -->
+		<!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"60px"}},"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group">
+			<!-- wp:separator {"className":"is-style-wide"} -->
+			<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+			<!-- /wp:separator -->
+		</div>
+		<!-- /wp:group -->
+		<!-- wp:paragraph {"className":"is-style-no-underline"} -->
+		<p class="is-style-no-underline"><a href="https://instagram.com/">Instagram</a></p>
+		<!-- /wp:paragraph -->
+		<!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"60px"}},"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group">
+			<!-- wp:separator {"className":"is-style-wide"} -->
+			<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+			<!-- /wp:separator -->
+		</div>
+		<!-- /wp:group -->
+		<!-- wp:paragraph {"className":"is-style-no-underline"} -->
+		<p class="is-style-no-underline"><a href="https://linkedin.com/">LinkedIn</a></p>
+		<!-- /wp:paragraph -->
+		<!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"60px"}},"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group">
+			<!-- wp:separator {"className":"is-style-wide"} -->
+			<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+			<!-- /wp:separator -->
+		</div>
+		<!-- /wp:group -->
+		<!-- wp:paragraph {"className":"is-style-no-underline"} -->
+		<p class="is-style-no-underline"><a href="https://www.facebook.com/">Facebook</a></p>
+		<!-- /wp:paragraph -->
+		<!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"60px"}},"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group">
+			<!-- wp:separator {"className":"is-style-wide"} -->
+			<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+			<!-- /wp:separator -->
+		</div>
+		<!-- /wp:group -->
+		<!-- wp:paragraph {"className":"is-style-no-underline"} -->
+		<p class="is-style-no-underline"><a href="https://www.tiktok.com/">TikTok</a></p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
