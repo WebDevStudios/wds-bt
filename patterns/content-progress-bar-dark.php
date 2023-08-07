a6f36bb4116b0428eb9acaa7b347be191d942e1a
Brian Gardner <bmg1227@gmail.com>
2023-08-07 08:00:27 -0500

    Add Progress Bar patterns.

---
+++ patterns/content-progress-bar-dark.php
@@ -0,0 +1,228 @@
+<?php
+/**
+ * Title: Section that shows progress bars.
+ * Slug: powder/content-progress-bar-dark
+ * Description: Section that shows progress bars.
+ * Categories: content
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-large","bottom":"var:preset|spacing|x-large","left":"30px","right":"30px"},"margin":{"top":"0"}}},"backgroundColor":"contrast","textColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-base-color has-contrast-background-color has-text-color has-background" style="margin-top:0;padding-top:var(--wp--preset--spacing--x-large);padding-right:30px;padding-bottom:var(--wp--preset--spacing--x-large);padding-left:30px">
+	<!-- wp:columns {"verticalAlignment":"bottom","isStackedOnMobile":false,"style":{"spacing":{"blockGap":{"top":"0","left":"0"}}}} -->
+	<div class="wp-block-columns are-vertically-aligned-bottom is-not-stacked-on-mobile">
+		<!-- wp:column {"verticalAlignment":"bottom","width":"100%","style":{"spacing":{"blockGap":"0"}}} -->
+		<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:100%">
+			<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"},"fontSize":"small"} -->
+			<div class="wp-block-group has-small-font-size">
+				<!-- wp:paragraph -->
+				<p>Item #1</p>
+				<!-- /wp:paragraph -->
+				<!-- wp:paragraph -->
+				<p>100%</p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+			<!-- wp:group {"backgroundColor":"base","layout":{"type":"constrained"}} -->
+			<div class="wp-block-group has-base-background-color has-background">
+				<!-- wp:spacer {"height":"20px"} -->
+				<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+				<!-- /wp:spacer -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+		<!-- wp:column {"verticalAlignment":"bottom","width":"0%","style":{"spacing":{"blockGap":"0"}}} -->
+		<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:0%">
+			<!-- wp:group {"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group">
+				<!-- wp:spacer {"height":"20px"} -->
+				<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+				<!-- /wp:spacer -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+	<!-- wp:columns {"verticalAlignment":"bottom","isStackedOnMobile":false,"style":{"spacing":{"blockGap":{"top":"0","left":"0"}}}} -->
+	<div class="wp-block-columns are-vertically-aligned-bottom is-not-stacked-on-mobile">
+		<!-- wp:column {"verticalAlignment":"bottom","width":"90%","style":{"spacing":{"blockGap":"0"}}} -->
+		<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:90%">
+			<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"},"fontSize":"small"} -->
+			<div class="wp-block-group has-small-font-size">
+				<!-- wp:paragraph -->
+				<p>Item #2</p>
+				<!-- /wp:paragraph -->
+				<!-- wp:paragraph -->
+				<p>90%</p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+			<!-- wp:group {"backgroundColor":"base","layout":{"type":"constrained"}} -->
+			<div class="wp-block-group has-base-background-color has-background">
+				<!-- wp:spacer {"height":"20px"} -->
+				<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+				<!-- /wp:spacer -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+		<!-- wp:column {"verticalAlignment":"bottom","width":"10%","style":{"spacing":{"blockGap":"0"}}} -->
+		<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:10%">
+			<!-- wp:group {"style":{"color":{"background":"#262626"}},"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group has-background" style="background-color:#262626">
+				<!-- wp:spacer {"height":"20px"} -->
+				<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+				<!-- /wp:spacer -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+	<!-- wp:columns {"verticalAlignment":"bottom","isStackedOnMobile":false,"style":{"spacing":{"blockGap":{"top":"0","left":"0"}}}} -->
+	<div class="wp-block-columns are-vertically-aligned-bottom is-not-stacked-on-mobile">
+		<!-- wp:column {"verticalAlignment":"bottom","width":"80%","style":{"spacing":{"blockGap":"0"}}} -->
+		<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:80%">
+			<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"},"fontSize":"small"} -->
+			<div class="wp-block-group has-small-font-size">
+				<!-- wp:paragraph -->
+				<p>Item #3</p>
+				<!-- /wp:paragraph -->
+				<!-- wp:paragraph -->
+				<p>80%</p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+			<!-- wp:group {"backgroundColor":"base","layout":{"type":"constrained"}} -->
+			<div class="wp-block-group has-base-background-color has-background">
+				<!-- wp:spacer {"height":"20px"} -->
+				<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+				<!-- /wp:spacer -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+		<!-- wp:column {"verticalAlignment":"bottom","width":"20%","style":{"spacing":{"blockGap":"0"}}} -->
+		<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:20%">
+			<!-- wp:group {"style":{"color":{"background":"#262626"}},"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group has-background" style="background-color:#262626">
+				<!-- wp:spacer {"height":"20px"} -->
+				<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+				<!-- /wp:spacer -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+	<!-- wp:columns {"verticalAlignment":"bottom","isStackedOnMobile":false,"style":{"spacing":{"blockGap":{"top":"0","left":"0"}}}} -->
+	<div class="wp-block-columns are-vertically-aligned-bottom is-not-stacked-on-mobile">
+		<!-- wp:column {"verticalAlignment":"bottom","width":"70%","style":{"spacing":{"blockGap":"0"}}} -->
+		<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:70%">
+			<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"},"fontSize":"small"} -->
+			<div class="wp-block-group has-small-font-size">
+				<!-- wp:paragraph -->
+				<p>Item #4</p>
+				<!-- /wp:paragraph -->
+				<!-- wp:paragraph -->
+				<p>70%</p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+			<!-- wp:group {"backgroundColor":"base","layout":{"type":"constrained"}} -->
+			<div class="wp-block-group has-base-background-color has-background">
+				<!-- wp:spacer {"height":"20px"} -->
+				<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+				<!-- /wp:spacer -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+		<!-- wp:column {"verticalAlignment":"bottom","width":"30%","style":{"spacing":{"blockGap":"0"}}} -->
+		<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:30%">
+			<!-- wp:group {"style":{"color":{"background":"#262626"}},"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group has-background" style="background-color:#262626">
+				<!-- wp:spacer {"height":"20px"} -->
+				<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+				<!-- /wp:spacer -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+	<!-- wp:columns {"verticalAlignment":"bottom","isStackedOnMobile":false,"style":{"spacing":{"blockGap":{"top":"0","left":"0"}}}} -->
+	<div class="wp-block-columns are-vertically-aligned-bottom is-not-stacked-on-mobile">
+		<!-- wp:column {"verticalAlignment":"bottom","width":"60%","style":{"spacing":{"blockGap":"0"}}} -->
+		<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:60%">
+			<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"},"fontSize":"small"} -->
+			<div class="wp-block-group has-small-font-size">
+				<!-- wp:paragraph -->
+				<p>Item #5</p>
+				<!-- /wp:paragraph -->
+				<!-- wp:paragraph -->
+				<p>60%</p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+			<!-- wp:group {"backgroundColor":"base","layout":{"type":"constrained"}} -->
+			<div class="wp-block-group has-base-background-color has-background">
+				<!-- wp:spacer {"height":"20px"} -->
+				<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+				<!-- /wp:spacer -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+		<!-- wp:column {"verticalAlignment":"bottom","width":"40%","style":{"spacing":{"blockGap":"0"}}} -->
+		<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:40%">
+			<!-- wp:group {"style":{"color":{"background":"#262626"}},"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group has-background" style="background-color:#262626">
+				<!-- wp:spacer {"height":"20px"} -->
+				<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+				<!-- /wp:spacer -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+	<!-- wp:columns {"verticalAlignment":"bottom","isStackedOnMobile":false,"style":{"spacing":{"blockGap":{"top":"0","left":"0"}}}} -->
+	<div class="wp-block-columns are-vertically-aligned-bottom is-not-stacked-on-mobile">
+		<!-- wp:column {"verticalAlignment":"bottom","width":"50%","style":{"spacing":{"blockGap":"0"}}} -->
+		<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:50%">
+			<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"},"fontSize":"small"} -->
+			<div class="wp-block-group has-small-font-size">
+				<!-- wp:paragraph -->
+				<p>Item #6</p>
+				<!-- /wp:paragraph -->
+				<!-- wp:paragraph -->
+				<p>50%</p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+			<!-- wp:group {"backgroundColor":"base","layout":{"type":"constrained"}} -->
+			<div class="wp-block-group has-base-background-color has-background">
+				<!-- wp:spacer {"height":"20px"} -->
+				<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+				<!-- /wp:spacer -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+		<!-- wp:column {"verticalAlignment":"bottom","width":"50%","style":{"spacing":{"blockGap":"0"}}} -->
+		<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:50%">
+			<!-- wp:group {"style":{"color":{"background":"#262626"}},"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group has-background" style="background-color:#262626">
+				<!-- wp:spacer {"height":"20px"} -->
+				<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+				<!-- /wp:spacer -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
