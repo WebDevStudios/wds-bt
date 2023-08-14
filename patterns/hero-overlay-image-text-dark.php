f0a5df0c906656ce0ab91d3670c04d2f1627f12a
Brian Gardner <bmg1227@gmail.com>
2023-08-14 16:22:05 -0500

    Better Group utility class.

--- patterns/hero-overlay-image-text-dark.php
+++ patterns/hero-overlay-image-text-dark.php
@@ -10,8 +10,8 @@
 	<!-- wp:image {"align":"wide","aspectRatio":"16/9","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
 	<figure class="wp-block-image alignwide size-full"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/sample-image-light.png'; ?>" alt="Sample image" style="aspect-ratio:16/9;object-fit:cover"/></figure>
 	<!-- /wp:image -->
-	<!-- wp:group {"style":{"spacing":{"margin":{"top":"0"}}},"className":"is-style-pull-200","layout":{"type":"constrained","wideSize":"800px","justifyContent":"left"}} -->
-	<div class="wp-block-group is-style-pull-200" style="margin-top:0">
+	<!-- wp:group {"className":"is-style-pull-200","layout":{"type":"constrained","wideSize":"800px","justifyContent":"left"}} -->
+	<div class="wp-block-group is-style-pull-200">
 		<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|large","right":"var:preset|spacing|small","bottom":"var:preset|spacing|large"},"margin":{"top":"0"}}},"backgroundColor":"contrast","textColor":"base","className":"is-style-position-relative","layout":{"type":"default"}} -->
 		<div class="wp-block-group is-style-position-relative has-base-color has-contrast-background-color has-text-color has-background" style="margin-top:0;padding-top:var(--wp--preset--spacing--large);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--large)">
 			<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"300","letterSpacing":"-2px"}},"fontSize":"max-72"} -->
