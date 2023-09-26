dfecd02212ef68a625944c9124fb0c3de63b99ca
Brian Gardner <bmg1227@gmail.com>
2023-09-26 09:15:11 -0500

    Consolidate function onto one line.

--- functions.php
+++ functions.php
@@ -37,12 +37,7 @@ add_action( 'after_setup_theme', 'powder_setup' );
  */
 function powder_enqueue_style_sheet() {
 
-	wp_enqueue_style(
-		'powder',
-		get_template_directory_uri() . '/style.css',
-		array(),
-		wp_get_theme( 'powder' )->get( 'Version' )
-	);
+	wp_enqueue_style( 'powder', get_template_directory_uri() . '/style.css', array(), wp_get_theme( 'powder' )->get( 'Version' ) );
 
 }
 add_action( 'wp_enqueue_scripts', 'powder_enqueue_style_sheet' );
