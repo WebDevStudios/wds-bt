b0646ea92dd922a4fb97b634ec9001a2e2729928
Brian Gardner <bmg1227@gmail.com>
2023-10-03 22:10:11 -0500

    Spaces in comments.

--- patterns/hidden-comments.php
+++ patterns/hidden-comments.php
@@ -14,28 +14,28 @@
 		<!-- wp:group {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|medium"}}}} -->
 		<div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--medium)">
 			<!-- wp:comment-template -->
-				<!-- wp:group {"tagName":"article","style":{"spacing":{"blockGap":"var:preset|spacing|x-small","margin":{"bottom":"var:preset|spacing|small"}}},"layout":{"type":"default"}} -->
-				<article class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--small)">
-					<!-- wp:group {"tagName":"header","style":{"spacing":{"blockGap":"10px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-					<header class="wp-block-group">
-						<!-- wp:avatar {"size":48,"style":{"border":{"radius":"24px"}}} /-->
-						<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"default"}} -->
+			<!-- wp:group {"tagName":"article","style":{"spacing":{"blockGap":"var:preset|spacing|x-small","margin":{"bottom":"var:preset|spacing|small"}}},"layout":{"type":"default"}} -->
+			<article class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--small)">
+				<!-- wp:group {"tagName":"header","style":{"spacing":{"blockGap":"10px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+				<header class="wp-block-group">
+					<!-- wp:avatar {"size":48,"style":{"border":{"radius":"24px"}}} /-->
+					<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"default"}} -->
+					<div class="wp-block-group">
+						<!-- wp:comment-author-name /-->
+						<!-- wp:group {"style":{"spacing":{"blockGap":"10px"}},"layout":{"type":"flex"}} -->
 						<div class="wp-block-group">
-							<!-- wp:comment-author-name /-->
-							<!-- wp:group {"style":{"spacing":{"blockGap":"10px"}},"layout":{"type":"flex"}} -->
-							<div class="wp-block-group">
-								<!-- wp:comment-date /-->
-								<!-- wp:comment-edit-link /-->
-							</div>
-							<!-- /wp:group -->
+							<!-- wp:comment-date /-->
+							<!-- wp:comment-edit-link /-->
 						</div>
 						<!-- /wp:group -->
-					</header>
+					</div>
 					<!-- /wp:group -->
-					<!-- wp:comment-content /-->
-					<!-- wp:comment-reply-link /-->
-				</article>
+				</header>
 				<!-- /wp:group -->
+				<!-- wp:comment-content /-->
+				<!-- wp:comment-reply-link /-->
+			</article>
+			<!-- /wp:group -->
 			<!-- /wp:comment-template -->
 		</div>
 		<!-- /wp:group -->
