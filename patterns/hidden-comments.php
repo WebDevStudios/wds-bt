bd14f61631b18a089c25e08447977aa65a8a7395
Brian Gardner <bmg1227@gmail.com>
2023-10-03 22:08:07 -0500

    Rebuild comments for optimized semantic markup.

--- patterns/hidden-comments.php
+++ patterns/hidden-comments.php
@@ -7,34 +7,35 @@
 ?>
 <!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|medium"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--medium)">
-	<!-- wp:comments {"className":"wp-block-comments-query-loop "} -->
+	<!-- wp:comments {"className":"wp-block-comments-query-loop"} -->
 	<div class="wp-block-comments wp-block-comments-query-loop">
+		<!-- wp:heading --><h2><?php echo esc_html__( 'Comments', 'powder' ); ?></h2><!-- /wp:heading -->
 		<!-- wp:comments-title {"level":3} /-->
 		<!-- wp:group {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|medium"}}}} -->
 		<div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--medium)">
 			<!-- wp:comment-template -->
-			<!-- wp:columns {"style":{"spacing":{"blockGap":"10px","margin":{"bottom":"30px"}}}} -->
-				<div class="wp-block-columns" style="margin-bottom:30px">
-				<!-- wp:column {"width":"48px"} -->
-				<div class="wp-block-column" style="flex-basis:48px">
-					<!-- wp:avatar {"size":48,"style":{"border":{"radius":"24px"}}} /-->
-				</div>
-				<!-- /wp:column -->
-				<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}}} -->
-				<div class="wp-block-column">
-					<!-- wp:comment-author-name /-->
-					<!-- wp:group {"style":{"spacing":{"margin":{"top":"0px"},"blockGap":"10px"}},"layout":{"type":"flex"}} -->
-					<div class="wp-block-group" style="margin-top:0px">
-						<!-- wp:comment-date /-->
-						<!-- wp:comment-edit-link /-->
-					</div>
+				<!-- wp:group {"tagName":"article","style":{"spacing":{"blockGap":"var:preset|spacing|x-small","margin":{"bottom":"var:preset|spacing|small"}}},"layout":{"type":"default"}} -->
+				<article class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--small)">
+					<!-- wp:group {"tagName":"header","style":{"spacing":{"blockGap":"10px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+					<header class="wp-block-group">
+						<!-- wp:avatar {"size":48,"style":{"border":{"radius":"24px"}}} /-->
+						<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"default"}} -->
+						<div class="wp-block-group">
+							<!-- wp:comment-author-name /-->
+							<!-- wp:group {"style":{"spacing":{"blockGap":"10px"}},"layout":{"type":"flex"}} -->
+							<div class="wp-block-group">
+								<!-- wp:comment-date /-->
+								<!-- wp:comment-edit-link /-->
+							</div>
+							<!-- /wp:group -->
+						</div>
+						<!-- /wp:group -->
+					</header>
 					<!-- /wp:group -->
 					<!-- wp:comment-content /-->
 					<!-- wp:comment-reply-link /-->
-				</div>
-				<!-- /wp:column -->
-			</div>
-			<!-- /wp:columns -->
+				</article>
+				<!-- /wp:group -->
 			<!-- /wp:comment-template -->
 		</div>
 		<!-- /wp:group -->
