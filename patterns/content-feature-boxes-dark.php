3ed080cd0b83ace3ab26d551915b81883932649e
Brian Gardner <bmg1227@gmail.com>
2023-10-10 06:25:08 -0500

    Add blocks to Feature Boxes patterns.

--- patterns/content-feature-boxes-dark.php
+++ patterns/content-feature-boxes-dark.php
@@ -7,6 +7,12 @@
 ?>
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-large","bottom":"var:preset|spacing|x-large","left":"30px","right":"30px"},"margin":{"top":"0"}}},"backgroundColor":"contrast","textColor":"base","layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull has-base-color has-contrast-background-color has-text-color has-background" style="margin-top:0;padding-top:var(--wp--preset--spacing--x-large);padding-right:30px;padding-bottom:var(--wp--preset--spacing--x-large);padding-left:30px">
+	<!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"lineHeight":"1"}},"fontSize":"max-48"} -->
+	<h1 class="wp-block-heading has-text-align-center has-max-48-font-size" style="line-height:1"><?php echo esc_html__( 'Choose Powder.', 'powder' ); ?></h1>
+	<!-- /wp:heading -->
+	<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"10px","bottom":"var:preset|spacing|small"}}}} -->
+	<p class="has-text-align-center" style="margin-top:10px;margin-bottom:var(--wp--preset--spacing--small)"><?php echo esc_html__( 'Redefine your space with a stylish, modern design.', 'powder' ); ?></p>
+	<!-- /wp:paragraph -->
 	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|small"}}}} -->
 	<div class="wp-block-columns alignwide">
 		<!-- wp:column {"style":{"border":{"color":"#262626","width":"1px"},"spacing":{"padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|small","right":"var:preset|spacing|small"},"blockGap":"var:preset|spacing|x-small"}}} -->
