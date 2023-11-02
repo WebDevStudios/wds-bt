b1f1f33b7734b52b8797b0cc8ddacdf808103c90
Brian Gardner <bmg1227@gmail.com>
2023-11-02 15:42:54 -0500

    Replace images, minor pattern styles.

--- patterns/template-page-boxed.php
+++ patterns/template-page-boxed.php
@@ -8,7 +8,7 @@
 ?>
 <!-- wp:group {"tagName":"header","style":{"spacing":{"blockGap":"10px","padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|x-large"},"margin":{"top":"0px"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"backgroundColor":"contrast","textColor":"base","className":"entry-header","layout":{"type":"constrained","wideSize":"800px"}} -->
 <header class="wp-block-group entry-header has-base-color has-contrast-background-color has-text-color has-background has-link-color" style="margin-top:0px;padding-top:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--x-large)">
-	<!-- wp:post-title {"textAlign":"center","level":1,"fontSize":"max-48"} /-->
+	<!-- wp:post-title {"textAlign":"center","level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"300"}},"fontSize":"max-48"} /-->
 </div>
 <!-- /wp:group -->
 <!-- wp:group {"align":"full","style":{"dimensions":{"minHeight":"100px"},"spacing":{"margin":{"top":"0"}}},"backgroundColor":"contrast","layout":{"type":"constrained"}} -->
