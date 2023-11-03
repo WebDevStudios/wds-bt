ca6470aec09373e4249756d02d49ddc42f6bc13a
Brian Gardner <bmg1227@gmail.com>
2023-11-02 19:05:45 -0500

    Better Posts patterns.

--- patterns/posts-grid-boxed.php
+++ patterns/posts-grid-boxed.php
@@ -6,19 +6,23 @@
  * Block Types: core/query
  */
 ?>
-<!-- wp:query {"queryId":0,"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"perPage":6},"align":"wide","layout":{"type":"default"}} -->
+<!-- wp:query {"queryId":0,"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"align":"wide","layout":{"type":"default"}} -->
 <div class="wp-block-query alignwide">
 	<!-- wp:post-template {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"grid","columnCount":3}} -->
 		<!-- wp:group {"className":"is-style-shadow-light","layout":{"type":"default"}} -->
 		<div class="wp-block-group is-style-shadow-light">
 			<!-- wp:group {"layout":{"type":"default"}} -->
 			<div class="wp-block-group">
-				<!-- wp:post-featured-image {"isLink":true} /-->
+				<!-- wp:post-featured-image {"isLink":true,"aspectRatio":"16/9"} /-->
 				<!-- wp:group {"style":{"spacing":{"blockGap":"10px","padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"},"margin":{"top":"0"}}}} -->
 					<div class="wp-block-group" style="margin-top:0;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)">
-					<!-- wp:post-title {"isLink":true,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"large"} /-->
-					<!-- wp:pattern {"slug":"powder/post-meta"} /-->
-					<!-- wp:post-excerpt {"moreText":"Read More →","fontSize":"small"} /-->
+					<!-- wp:group {"tagName":"header","style":{"spacing":{"blockGap":"10px"}},"className":"entry-header","layout":{"type":"default"}} -->
+					<header class="wp-block-group entry-header">
+						<!-- wp:post-title {"isLink":true,"fontSize":"large"} /-->
+						<!-- wp:pattern {"slug":"powder/post-meta"} /-->
+					</header>
+					<!-- /wp:group -->
+					<!-- wp:post-excerpt {"moreText":"Read More →","excerptLength":20,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}}},"fontSize":"small"} /-->
 				</div>
 				<!-- /wp:group -->
 			</div>
--- patterns/posts-grid.php
+++ patterns/posts-grid.php
@@ -6,7 +6,7 @@
  * Block Types: core/query
  */
 ?>
-<!-- wp:query {"queryId":0,"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"perPage":"10"},"align":"wide","layout":{"type":"default"}} -->
+<!-- wp:query {"queryId":0,"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"align":"wide","layout":{"type":"default"}} -->
 <div class="wp-block-query alignwide">
 	<!-- wp:post-template {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"grid","columnCount":3}} -->
 		<!-- wp:group {"tagName":"article","layout":{"type":"default"}} -->
@@ -14,13 +14,13 @@
 		<!-- wp:post-featured-image {"isLink":true,"aspectRatio":"16/9"} /-->
 			<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|x-small","margin":{"top":"30px"}}}} -->
 			<div class="wp-block-group" style="margin-top:30px">
-				<!-- wp:group {"tagName":"header","style":{"spacing":{"blockGap":"10px"}},"className":"entry-header","layout":{"type":"constrained"}} -->
+				<!-- wp:group {"tagName":"header","style":{"spacing":{"blockGap":"10px"}},"className":"entry-header","layout":{"type":"default"}} -->
 				<header class="wp-block-group entry-header">
 					<!-- wp:post-title {"isLink":true,"fontSize":"large"} /-->
 					<!-- wp:pattern {"slug":"powder/post-meta"} /-->
 				</header>
 				<!-- /wp:group -->
-				<!-- wp:post-excerpt {"moreText":"Read More →","excerptLength":20,"fontSize":"small"} /-->
+				<!-- wp:post-excerpt {"moreText":"Read More →","excerptLength":20,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}}},"fontSize":"small"} /-->
 			</div>
 			<!-- /wp:group -->
 		</article>
