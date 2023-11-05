a1f3b896084a2435d961cc9397e42bddcfb64f4a
Brian Gardner <bmg1227@gmail.com>
2023-11-04 21:38:59 -0500

    Add Powder Social home page pattern.

---
+++ patterns/page-home-social.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Home page for Powder Social
+ * Slug: powder/page-home-social
+ * Categories: page
+ */
+?>
+<!-- wp:pattern {"slug":"powder/hero-stacked-text-image-dark"} /-->
+<!-- wp:pattern {"slug":"powder/hero-columns-text-image"} /-->
+<!-- wp:pattern {"slug":"powder/content-social-numbers-dark"} /-->
+<!-- wp:pattern {"slug":"powder/hero-columns-image-text"} /-->
+<!-- wp:pattern {"slug":"powder/content-logos-dark"} /-->
+<!-- wp:pattern {"slug":"powder/hero-columns-text-image"} /-->
