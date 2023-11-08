92c38d17bdc2afef019dd7c14bb05168f1995109
Brian Gardner <bmg1227@gmail.com>
2023-11-07 21:56:18 -0600

    Add Business Home Page pattern.

---
+++ patterns/page-home-business.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Home page for Powder Business
+ * Slug: powder/page-home-business
+ * Categories: page
+ */
+?>
+<!-- wp:pattern {"slug":"powder/hero-gradient-cover"} /-->
+<!-- wp:pattern {"slug":"powder/hero-columns-image-text"} /-->
+<!-- wp:pattern {"slug":"powder/content-cta-stacked-dark"} /-->
+<!-- wp:pattern {"slug":"powder/content-feature-columns"} /-->
+<!-- wp:pattern {"slug":"powder/hero-columns-text-image-dark"} /-->
+<!-- wp:pattern {"slug":"powder/content-ebook-covers"} /-->
