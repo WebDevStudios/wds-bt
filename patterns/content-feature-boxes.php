bbf4eb86310bca874855afcf15c8f556be5de142
Brian Gardner <bmg1227@gmail.com>
2023-11-17 17:41:18 -0600

    Add columns to Feature Boxes patterns.

--- patterns/content-feature-boxes.php
+++ patterns/content-feature-boxes.php
@@ -56,5 +56,48 @@
 		<!-- /wp:column -->
 	</div>
 	<!-- /wp:columns -->
+	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|small"},"margin":{"top":"var:preset|spacing|small"}}}} -->
+	<div class="wp-block-columns alignwide" style="margin-top:var(--wp--preset--spacing--small)">
+		<!-- wp:column {"style":{"border":{"color":"#e5e5e5","width":"1px"},"spacing":{"padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|small","right":"var:preset|spacing|small"},"blockGap":"var:preset|spacing|x-small"}}} -->
+		<div class="wp-block-column has-border-color" style="border-color:#e5e5e5;border-width:1px;padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--small)">
+			<!-- wp:image {"align":"center","width":30,"height":30,"scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/icon-circle-bolt-dark.svg'; ?>" alt="Circle check dark icon" style="object-fit:cover;width:30px;height:30px" width="30" height="30"/></figure>
+			<!-- /wp:image -->
+			<!-- wp:heading {"textAlign":"center","style":{"typography":{"textTransform":"uppercase"}},"fontSize":"x-small"} -->
+			<h2 class="wp-block-heading has-text-align-center has-x-small-font-size" style="text-transform:uppercase"><?php echo esc_html__( 'Feature Headline', 'powder' ); ?></h2>
+			<!-- /wp:heading -->
+			<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+			<p class="has-text-align-center has-small-font-size"><?php echo esc_html__( 'Design beautiful WordPress websites with the stylish Powder theme.', 'powder' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+		<!-- wp:column {"style":{"border":{"color":"#e5e5e5","width":"1px"},"spacing":{"padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|small","right":"var:preset|spacing|small"},"blockGap":"var:preset|spacing|x-small"}}} -->
+		<div class="wp-block-column has-border-color" style="border-color:#e5e5e5;border-width:1px;padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--small)">
+			<!-- wp:image {"align":"center","width":30,"height":30,"scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/icon-circle-bolt-dark.svg'; ?>" alt="Circle check dark icon" style="object-fit:cover;width:30px;height:30px" width="30" height="30"/></figure>
+			<!-- /wp:image -->
+			<!-- wp:heading {"textAlign":"center","style":{"typography":{"textTransform":"uppercase"}},"fontSize":"x-small"} -->
+			<h2 class="wp-block-heading has-text-align-center has-x-small-font-size" style="text-transform:uppercase"><?php echo esc_html__( 'Feature Headline', 'powder' ); ?></h2>
+			<!-- /wp:heading -->
+			<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+			<p class="has-text-align-center has-small-font-size"><?php echo esc_html__( 'Design beautiful WordPress websites with the stylish Powder theme.', 'powder' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+		<!-- wp:column {"style":{"border":{"color":"#e5e5e5","width":"1px"},"spacing":{"padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|small","right":"var:preset|spacing|small"},"blockGap":"var:preset|spacing|x-small"}}} -->
+		<div class="wp-block-column has-border-color" style="border-color:#e5e5e5;border-width:1px;padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--small)">
+			<!-- wp:image {"align":"center","width":30,"height":30,"scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/icon-circle-bolt-dark.svg'; ?>" alt="Circle check dark icon" style="object-fit:cover;width:30px;height:30px" width="30" height="30"/></figure>
+			<!-- /wp:image -->
+			<!-- wp:heading {"textAlign":"center","style":{"typography":{"textTransform":"uppercase"}},"fontSize":"x-small"} -->
+			<h2 class="wp-block-heading has-text-align-center has-x-small-font-size" style="text-transform:uppercase"><?php echo esc_html__( 'Feature Headline', 'powder' ); ?></h2>
+			<!-- /wp:heading -->
+			<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+			<p class="has-text-align-center has-small-font-size"><?php echo esc_html__( 'Design beautiful WordPress websites with the stylish Powder theme.', 'powder' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
 </div>
 <!-- /wp:group -->
