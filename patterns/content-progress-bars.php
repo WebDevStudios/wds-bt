a26d99ae188e955dbff97f91aceb89823411fb9f
Brian Gardner <bmg1227@gmail.com>
2023-11-19 15:17:40 -0600

    Trim Progress Bars patterns.

--- patterns/content-progress-bars.php
+++ patterns/content-progress-bars.php
@@ -187,41 +187,5 @@
 		<!-- /wp:column -->
 	</div>
 	<!-- /wp:columns -->
-	<!-- wp:columns {"verticalAlignment":"bottom","isStackedOnMobile":false,"style":{"spacing":{"blockGap":{"top":"0","left":"0"}}}} -->
-	<div class="wp-block-columns are-vertically-aligned-bottom is-not-stacked-on-mobile">
-		<!-- wp:column {"verticalAlignment":"bottom","width":"50%","style":{"spacing":{"blockGap":"0"}}} -->
-		<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:50%">
-			<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"},"fontSize":"small"} -->
-			<div class="wp-block-group has-small-font-size">
-				<!-- wp:paragraph -->
-				<p><?php echo esc_html__( 'Item #6', 'powder' ); ?></p>
-				<!-- /wp:paragraph -->
-				<!-- wp:paragraph -->
-				<p><?php echo esc_html__( '50%', 'powder' ); ?></p>
-				<!-- /wp:paragraph -->
-			</div>
-			<!-- /wp:group -->
-			<!-- wp:group {"backgroundColor":"contrast","layout":{"type":"constrained"}} -->
-			<div class="wp-block-group has-contrast-background-color has-background">
-				<!-- wp:spacer {"height":"20px"} -->
-				<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
-				<!-- /wp:spacer -->
-			</div>
-			<!-- /wp:group -->
-		</div>
-		<!-- /wp:column -->
-		<!-- wp:column {"verticalAlignment":"bottom","width":"50%","style":{"spacing":{"blockGap":"0"}}} -->
-		<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:50%">
-			<!-- wp:group {"style":{"color":{"background":"#e5e5e5"}},"layout":{"type":"constrained"}} -->
-			<div class="wp-block-group has-background" style="background-color:#e5e5e5">
-				<!-- wp:spacer {"height":"20px"} -->
-				<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
-				<!-- /wp:spacer -->
-			</div>
-			<!-- /wp:group -->
-		</div>
-		<!-- /wp:column -->
-	</div>
-	<!-- /wp:columns -->
 </div>
 <!-- /wp:group -->
