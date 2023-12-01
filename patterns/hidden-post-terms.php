7a604f61eec1b14a20cb351cf244a426191a90fe
Brian Gardner <bmg1227@gmail.com>
2023-12-01 08:13:38 -0600

    Make text in Post Terms pattern translatable.

--- patterns/hidden-post-terms.php
+++ patterns/hidden-post-terms.php
@@ -7,7 +7,7 @@
 ?>
 <!-- wp:group {"tagName":"footer","style":{"spacing":{"blockGap":"0px"}},"className":"entry-footer","layout":{"type":"constrained"}} -->
 <footer class="wp-block-group entry-footer">
-	<!-- wp:post-terms {"term":"category","prefix":"In: "} /-->
-	<!-- wp:post-terms {"term":"post_tag","prefix":"Tags: "} /-->
+	<!-- wp:post-terms {"term":"category","prefix":"<?php echo esc_html__( 'In:', 'powder' ); ?> "} /-->
+	<!-- wp:post-terms {"term":"post_tag","prefix":"<?php echo esc_html__( 'Tags:', 'powder' ); ?> "} /-->
 </footer>
 <!-- /wp:group -->
