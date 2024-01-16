f3520e7b39d51c158567ec01f05c3d0a2629f48c
JC <jc@webdevstudios.com>
2024-01-17 02:39:09 +0800

    Cleanup code

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,26 +1,28 @@
 // Add the .js-enabled class to the <body> when JavaScript is detected
-document.addEventListener('DOMContentLoaded', () => {
-	document.body.classList.add('js-enabled');
-});
+document.addEventListener( 'DOMContentLoaded', () => {
+	document.body.classList.add( 'js-enabled' );
+} );
 
 // Cache the header element to avoid re-querying the DOM on each scroll
-const powderHeader = document.querySelector(".site-header");
+const powderHeader = document.querySelector( '.site-header' );
 
 // Initialize the last scroll top position
 let powderLastScrollTop = 0;
 
 // Function to handle the scroll event
 function handleScroll() {
-	if (powderHeader) { // Only proceed if the header exists
-		const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+	if ( powderHeader ) {
+		// Only proceed if the header exists
+		const scrollTop =
+			window.pageYOffset || document.documentElement.scrollTop;
 
 		// Compare the new scroll position with the last scroll position
-		if (scrollTop > powderLastScrollTop) {
+		if ( scrollTop > powderLastScrollTop ) {
 			// If scrolling down, hide the header
-			powderHeader.style.top = "-100%";
+			powderHeader.style.top = '-100%';
 		} else {
 			// If scrolling up, show the header
-			powderHeader.style.top = "0";
+			powderHeader.style.top = '0';
 		}
 
 		// Update the last scroll position
@@ -32,14 +34,14 @@ function handleScroll() {
 let isThrottled = false;
 const powderThrottleDuration = 100; // milliseconds
 
-window.addEventListener("scroll", () => {
-	if (!isThrottled) {
+window.addEventListener( 'scroll', () => {
+	if ( ! isThrottled ) {
 		handleScroll();
 		isThrottled = true;
 
 		// Set a timeout to un-throttle after a certain duration
-		setTimeout(() => {
+		setTimeout( () => {
 			isThrottled = false;
-		}, powderThrottleDuration);
+		}, powderThrottleDuration );
 	}
-});
+} );
--- assets/js/variations.js
+++ assets/js/variations.js
@@ -1,4 +1,3 @@
-
 /**
  * Add Block Variations.
  * Example:
