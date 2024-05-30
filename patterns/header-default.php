f967d86a655d1b609858793b2df45d9a7b516194
JC <jc@webdevstudios.com>
2024-05-31 04:22:18 +0800

    Lock patterns to just content

--- patterns/header-default.php
+++ patterns/header-default.php
@@ -10,10 +10,9 @@
 
 // Determine whether to display site logo or site title.
 $wds_site_info = has_custom_logo() ? '<!-- wp:site-logo {"width":150,"shouldSyncIcon":true,"style":{"layout":{"selfStretch":"fit","flexSize":null}}} /-->' : '<!-- wp:site-title /-->';
-
 ?>
 
-<!-- wp:group {"metadata":{"name":"Header"},"align":"full","style":{"spacing":{"margin":{"top":"0px"},"padding":{"top":"30px","bottom":"30px"}}},"backgroundColor":"white","layout":{"type":"constrained"}} -->
+<!-- wp:group {"templateLock":"contentOnly","metadata":{"name":"Header"},"align":"full","style":{"spacing":{"margin":{"top":"0px"},"padding":{"top":"30px","bottom":"30px"}}},"backgroundColor":"white","layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull has-white-background-color has-background" style="margin-top:0px;padding-top:30px;padding-bottom:30px">
 
 	<!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
