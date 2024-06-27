a36838ef7b3a1bf150c53b7e486202d2aea53517
JC <jc@webdevstudios.com>
2024-06-28 02:16:52 +0800

    WDSBT-23 - Update Single template with patterns and related posts pattern from register_block_pattern

--- inc/hooks/register-block-patterns.php
+++ inc/hooks/register-block-patterns.php
@@ -18,6 +18,7 @@ function register_custom_block_pattern() {
 				'title'         => __( 'Related Posts', 'wdsbt' ),
 				'blockTypes'    => array( 'core/query' ),
 				'templateTypes' => array( 'single-post' ),
+				'postTypes'     => array( '' ),
 				'description'   => _x( 'Related Posts query variation', 'Block pattern description', 'wdsbt' ),
 				'content'       => "<!-- wp:group {\"tagName\":\"aside\",\"metadata\":{\"name\":\"Recent Posts\"},\"style\":{\"spacing\":{\"margin\":{\"top\":\"var:preset|spacing|40\"},\"padding\":{\"bottom\":\"var:preset|spacing|50\"}}},\"layout\":{\"type\":\"constrained\"}} -->\n<aside class=\"wp-block-group\" style=\"margin-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--50)\"><!-- wp:query {\"queryId\":0,\"query\":{\"perPage\":10,\"pages\":0,\"offset\":0,\"postType\":\"post\",\"order\":\"desc\",\"orderBy\":\"date\",\"author\":\"\",\"search\":\"\",\"exclude\":[],\"sticky\":\"\",\"inherit\":true,\"taxQuery\":null,\"parents\":[]}} -->\n<div class=\"wp-block-query\"><!-- wp:post-template {\"className\":\"related-posts-query\",\"layout\":{\"type\":\"grid\",\"columnCount\":3}} -->\n<!-- wp:post-featured-image {\"isLink\":true} /-->\n\n<!-- wp:post-terms {\"term\":\"category\"} /-->\n\n<!-- wp:post-title {\"isLink\":true,\"fontSize\":\"small\"} /-->\n\n<!-- wp:read-more {\"content\":\"\"} /-->\n<!-- /wp:post-template --></div>\n<!-- /wp:query --></aside>\n<!-- /wp:group -->",
 			)
