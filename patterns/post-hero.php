481f1823ee9a07ac8ac2779e59d9947e07458e45
JC <jc@webdevstudios.com>
2024-07-16 05:04:35 +0800

    WDSBT-23 - Fix font error, and style post meta

--- patterns/post-hero.php
+++ patterns/post-hero.php
@@ -12,15 +12,25 @@
 ?>
 
 <!-- wp:group {"metadata":{"name":"Hero"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}}},"backgroundColor":"primary-50","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull has-primary-50-background-color has-background" style="padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70)"><!-- wp:columns {"verticalAlignment":null,"align":"wide"} -->
-<div class="wp-block-columns alignwide"><!-- wp:column {"verticalAlignment":"top","layout":{"type":"constrained"}} -->
-<div class="wp-block-column is-vertically-aligned-top"><!-- wp:post-terms {"term":"category","prefix":"\u003cbr\u003e"} /-->
+<div class="wp-block-group alignfull has-primary-50-background-color has-background" style="padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70)">
 
-<!-- wp:post-title /--></div>
-<!-- /wp:column -->
+	<!-- wp:columns {"verticalAlignment":null,"align":"wide"} -->
+	<div class="wp-block-columns alignwide">
 
-<!-- wp:column {"verticalAlignment":"center","layout":{"type":"constrained"}} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:post-featured-image {"aspectRatio":"16/9","style":{"border":{"radius":"24px"}}} /--></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns --></div>
+		<!-- wp:column {"verticalAlignment":"top","layout":{"type":"constrained"}} -->
+		<div class="wp-block-column is-vertically-aligned-top">
+			<!-- wp:post-terms {"term":"category","prefix":""} /-->
+			<!-- wp:post-title /-->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"verticalAlignment":"center","layout":{"type":"constrained"}} -->
+		<div class="wp-block-column is-vertically-aligned-center">
+			<!-- wp:post-featured-image {"aspectRatio":"16/9","style":{"border":{"radius":"24px"}}} /-->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+
+</div>
 <!-- /wp:group -->
