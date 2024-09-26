717b8214b4ad961757f4680bf65dba0c133a5b0e
JC <jc@webdevstudios.com>
2024-09-27 03:16:19 +0800

    Add cache buster script

--- inc/setup/scripts.php
+++ inc/setup/scripts.php
@@ -14,18 +14,19 @@ namespace WebDevStudios\wdsbt;
  */
 function scripts() {
 	$asset_file_path = get_template_directory() . '/build/js/index.asset.php';
+	$asset_version   = wp_get_theme()->get( 'Version' );
 
 	if ( is_readable( $asset_file_path ) ) {
 		$asset_file = include $asset_file_path;
 	} else {
-		$asset_file = [
+		$asset_file = array(
 			'version'      => '0.1.0',
-			'dependencies' => [ 'wp-polyfill' ],
-		];
+			'dependencies' => array( 'wp-polyfill' ),
+		);
 	}
 
 	// Register styles & scripts.
-	wp_enqueue_style( 'wdsbt-styles', get_stylesheet_directory_uri() . '/build/css/style.css', [], $asset_file['version'] );
-	wp_enqueue_script( 'wdsbt-scripts', get_stylesheet_directory_uri() . '/build/js/index.js', $asset_file['dependencies'], $asset_file['version'], true );
+	wp_enqueue_style( 'freeman-styles', get_stylesheet_directory_uri() . '/build/css/style.css', array(), $asset_version );
+	wp_enqueue_script( 'freeman-scripts', get_stylesheet_directory_uri() . '/build/js/index.js', $asset_file['dependencies'], $asset_version, true );
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\scripts' );
