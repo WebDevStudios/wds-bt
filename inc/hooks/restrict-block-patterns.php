706f4fca9a7bea6b0a0bf24362061e3078acda20
JC <jc@webdevstudios.com>
2024-10-09 05:02:31 +0800

    Hotfix - restrict patterns to theme only

---
+++ inc/hooks/restrict-block-patterns.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Restrict block patterns to theme-specific patterns only.
+ *
+ * @package wdsbt
+ */
+
+namespace WebDevStudios\wdsbt;
+
+/**
+ * Restrict patterns from the Dotcom library by overriding the patterns source site.
+ *
+ * @return string The string to disable the Dotcom patterns source.
+ */
+function restrict_block_editor_patterns() {
+	return 'disable-dotcom-patterns-source';
+}
+add_filter( 'a8c_override_patterns_source_site', __NAMESPACE__ . '\restrict_block_editor_patterns' );
+
+// Disable the loading of remote patterns from the Dotorg pattern directory.
+add_filter( 'should_load_remote_block_patterns', '__return_false' );
