3d3bd86379963b47cbaf6a0968604ff47e0ebef8
Jennifer Farhat <jennifer.farhat@webdevstudios.com>
2025-04-24 07:26:41 -0700

    car 22

--- patterns/card-3.php
+++ patterns/card-3.php
@@ -11,8 +11,8 @@
 <!-- wp:group {"metadata":{"name":"Card #3","categories":["Cards"],"patternName":"wdsbt/card-3"},"style":{"shadow":"var:preset|shadow|light-400","spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"blockGap":"0"},"border":{"radius":"10px"}},"backgroundColor":"white","layout":{"type":"default"}} -->
 <div class="wp-block-group has-white-background-color has-background" style="border-radius:10px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;box-shadow:var(--wp--preset--shadow--light-400)"><!-- wp:post-featured-image {"isLink":true,"aspectRatio":"3/2","overlayColor":"black-alpha-50","dimRatio":50,"style":{"border":{"radius":{"topLeft":"6px","topRight":"6px"}}}} /-->
 
-	<!-- wp:group {"style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"padding":{"top":"1.5rem","bottom":"1.5rem","right":"1.5rem","left":"1.5rem"},"blockGap":"0.5rem"}},"layout":{"type":"constrained"}} -->
-	<div class="wp-block-group" style="padding-top:1.5rem;padding-right:1.5rem;padding-bottom:1.5rem;padding-left:1.5rem"><!-- wp:post-title {"style":{"typography":{"fontStyle":"normal","fontWeight":"600","lineHeight":"1.3"}},"fontSize":"l"} /-->
+	<!-- wp:group {"style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"padding":{"top":"1.5rem","bottom":"1.5rem","right":"1.5rem","left":"1.5rem"},"blockGap":"0.5rem","margin":{"top":"0"}}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group" style="margin-top:0;padding-top:1.5rem;padding-right:1.5rem;padding-bottom:1.5rem;padding-left:1.5rem"><!-- wp:post-title {"style":{"typography":{"fontStyle":"normal","fontWeight":"600","lineHeight":"1.3"}},"fontSize":"l"} /-->
 
 		<!-- wp:post-excerpt {"showMoreOnNewLine":false,"style":{"elements":{"link":{"color":{"text":"var:preset|color|grey-500"}}}},"textColor":"grey-500"} /-->
 
