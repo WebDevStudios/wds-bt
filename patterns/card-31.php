5400d1a1bdf4977ac5e23ba892f978d2653db168
Jennifer Farhat <jennifer.farhat@webdevstudios.com>
2025-04-24 10:23:47 -0700

    Card #31

---
+++ patterns/card-31.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: Card #31
+ * Slug: wdsbt/card-31
+ * Categories: Cards
+ *
+ * @package wdsbt
+ */
+
+?>
+<!-- wp:group {"metadata":{"name":"Card #31","categories":["Cards"],"patternName":"wdsbt/card-31"},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"overlayColor":"black-alpha-50","isUserOverlayColor":true,"contentPosition":"center left","style":{"border":{"radius":"10px"},"dimensions":{"aspectRatio":"4/3"},"shadow":"var:preset|shadow|light-400","elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"left":"1.5rem","right":"1.5rem","top":"1.5rem","bottom":"1.5rem"},"blockGap":"0"}},"textColor":"white","layout":{"type":"default"}} -->
+	<div class="wp-block-cover has-custom-content-position is-position-center-left has-white-color has-text-color has-link-color" style="border-radius:10px;padding-top:1.5rem;padding-right:1.5rem;padding-bottom:1.5rem;padding-left:1.5rem;box-shadow:var(--wp--preset--shadow--light-400)"><span aria-hidden="true" class="wp-block-cover__background has-black-alpha-50-background-color has-background-dim"></span>
+		<div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"0.5rem","margin":{"top":"0"}}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"bottom"}} -->
+			<div class="wp-block-group" style="margin-top:0"><!-- wp:post-title {"style":{"typography":{"fontStyle":"normal","fontWeight":"600","lineHeight":"1.27"}},"fontSize":"l"} /--></div>
+			<!-- /wp:group -->
+		</div>
+	</div>
+	<!-- /wp:cover -->
+</div>
+<!-- /wp:group -->
