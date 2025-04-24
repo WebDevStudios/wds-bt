db53bf7c00ef012fa33a78956dd002a9506191c1
Lindsey Bell <lindsey.bell@webdevstudios.com>
2025-04-24 15:11:31 -0400

    Localization

--- patterns/card-30.php
+++ patterns/card-30.php
@@ -9,14 +9,14 @@
 
 ?>
 
-<!-- wp:cover {"url":"http://wds-bt.local/wp-content/uploads/2025/04/placeholder-350x510-1.png","dimRatio":0,"customOverlayColor":"#dbdbdb","isUserOverlayColor":false,"isDark":false,"sizeSlug":"full","style":{"border":{"radius":"10px"},"shadow":"var:preset|shadow|elevation-400"}} -->
-<div class="wp-block-cover is-light" style="border-radius:10px;box-shadow:var(--wp--preset--shadow--elevation-400)"><img class="wp-block-cover__image-background  size-full" alt="" src="http://wds-bt.local/wp-content/uploads/2025/04/placeholder-350x510-1.png" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim" style="background-color:#dbdbdb"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"dimensions":{"minHeight":"510px"},"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"bottom"}} -->
+<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/placeholder-350x510-1.png","dimRatio":0,"customOverlayColor":"#dbdbdb","isUserOverlayColor":false,"isDark":false,"sizeSlug":"full","style":{"border":{"radius":"10px"},"shadow":"var:preset|shadow|elevation-400"}} -->
+<div class="wp-block-cover is-light" style="border-radius:10px;box-shadow:var(--wp--preset--shadow--elevation-400)"><img class="wp-block-cover__image-background  size-full" alt="" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/placeholder-350x510-1.png" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim" style="background-color:#dbdbdb"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"dimensions":{"minHeight":"510px"},"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"bottom"}} -->
 <div class="wp-block-group" style="min-height:510px"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"#ffffffb3"}}},"color":{"text":"#ffffffb3"}}} -->
-<p class="has-text-color has-link-color" style="color:#ffffffb3">Subtitle</p>
+<p class="has-text-color has-link-color" style="color:#ffffffb3"><?php esc_html_e( 'Subtitle', 'wdsbt' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
-<h2 class="wp-block-heading has-white-color has-text-color has-link-color">Title</h2>
+<h2 class="wp-block-heading has-white-color has-text-color has-link-color"><?php esc_html_e( 'Title', 'wdsbt' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:buttons {"style":{"spacing":{"margin":{"top":"1.5rem"}}}} -->
