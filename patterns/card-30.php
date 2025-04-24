48fcd61dc183919c209c432d470e264a981a7f9e
Lindsey Bell <lindsey.bell@webdevstudios.com>
2025-04-24 15:06:30 -0400

    add Card #30

---
+++ patterns/card-30.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Title: Card #30
+ * Slug: wdsbt/card-30
+ * Categories: Cards
+ *
+ * @package wdsbt
+ */
+
+?>
+
+<!-- wp:cover {"url":"http://wds-bt.local/wp-content/uploads/2025/04/placeholder-350x510-1.png","dimRatio":0,"customOverlayColor":"#dbdbdb","isUserOverlayColor":false,"isDark":false,"sizeSlug":"full","style":{"border":{"radius":"10px"},"shadow":"var:preset|shadow|elevation-400"}} -->
+<div class="wp-block-cover is-light" style="border-radius:10px;box-shadow:var(--wp--preset--shadow--elevation-400)"><img class="wp-block-cover__image-background  size-full" alt="" src="http://wds-bt.local/wp-content/uploads/2025/04/placeholder-350x510-1.png" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim" style="background-color:#dbdbdb"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"dimensions":{"minHeight":"510px"},"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"bottom"}} -->
+<div class="wp-block-group" style="min-height:510px"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"#ffffffb3"}}},"color":{"text":"#ffffffb3"}}} -->
+<p class="has-text-color has-link-color" style="color:#ffffffb3">Subtitle</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<h2 class="wp-block-heading has-white-color has-text-color has-link-color">Title</h2>
+<!-- /wp:heading -->
+
+<!-- wp:buttons {"style":{"spacing":{"margin":{"top":"1.5rem"}}}} -->
+<div class="wp-block-buttons" style="margin-top:1.5rem"><!-- wp:button {"className":"is-style-fill"} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link wp-element-button">Button</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
